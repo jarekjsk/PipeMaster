12675d3efc02aab9fde57f485e7451a0096a01bb
jarekjsk <j.szczepaniak93@gmail.com>
2025-03-15 22:30:41 +0100

    initial environment setup and folder structure

---
+++ PipeMaster/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+function App() {
+
+
+  return (
+    <Router>
+      {/* <Navbar /> */}
+      <Routes>
+
+            {/* <Route path="/" element={} /> */}
+            {/* <Route path="/" element={} /> */}
+
+      </Routes>
+      {/* <Footer /> */}
+    </Router>
+  )
+}
+
+export default App
